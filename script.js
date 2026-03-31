9f756332e4ec147facf606fb92a76ae5af3c25fc
FJMalfi <260198491+f4malfi@users.noreply.github.com>
2026-03-31 03:37:30 -0400

    Added global variables for score Added human choice function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//
+//This is the computer logic which randomizes it's choice
 
 let max = 3;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     let computerNumber = Math.floor(Math.random() * max);
@@ -15,4 +17,12 @@ function getComputerChoice(){
     }
 };
 
-console.log(getComputerChoice ());
+//Function to prompt the users choice
+
+function getHumanChoice(response){
+    response = prompt("Rock, Paper or Scissors?");
+}
+
+// console.log(getHumanChoice());
+
+// function playRound()
