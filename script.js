6ef02a126a170d506b5d74c1b0666a41ed6ba927
FJMalfi <260198491+f4malfi@users.noreply.github.com>
2026-03-30 23:31:16 -0400

    Added rock randomizer for computer player

---
+++ script.js
@@ -0,0 +1,14 @@
+//
+
+let max = 2;
+
+function getComputerChoice(){
+    let computerNumber = Math.floor(Math.random() * max);
+    if (computerNumber === 1){
+        return("Rock")
+    }
+
+
+    };
+
+console.log(getComputerChoice ());
