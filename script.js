2d784c0f9533bf6dee86fd8410888442e5fb7eda
FJMalfi <260198491+f4malfi@users.noreply.github.com>
2026-03-31 03:22:09 -0400

    Added computer logic

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 //
 
-let max = 2;
+let max = 3;
 
 function getComputerChoice(){
     let computerNumber = Math.floor(Math.random() * max);
-    if (computerNumber === 1){
+    if (computerNumber === 0){
         return("Rock")
     }
-
-
-    };
+    else if (computerNumber === 1){
+        return ("Scissors")
+    }
+    else if (computerNumber === 2){
+        return ("Paper")
+    }
+};
 
 console.log(getComputerChoice ());
