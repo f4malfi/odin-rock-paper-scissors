78a83306a6a890fc17f10834e1b29de1d68a818e
FJMalfi <260198491+f4malfi@users.noreply.github.com>
2026-04-01 04:06:51 -0400

    Added round logic and need to add full game mode

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
-//This is the computer logic which randomizes it's choice
+//Global Variables
 
-let max = 3;
 let humanScore = 0;
 let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+//This is the computer logic which randomizes it's choice
 
 function getComputerChoice(){
+    let max = 3;
     let computerNumber = Math.floor(Math.random() * max);
+
     if (computerNumber === 0){
-        return("Rock")
+        return("rock")
     }
     else if (computerNumber === 1){
-        return ("Scissors")
+        return ("scissors")
     }
     else if (computerNumber === 2){
-        return ("Paper")
+        return ("paper")
     }
 };
 
+//let computerChoice = getComputerChoice();
+
 //Function to prompt the users choice
 
-function getHumanChoice(response){
-    response = prompt("Rock, Paper or Scissors?");
-}
+function getHumanChoice(){
+     return prompt("rock, paper or scissors?");
+ }
 
-// console.log(getHumanChoice());
+//let humanChoice = getHumanChoice();
+// //Function to simulate 1 round
 
-// function playRound()
+function playRound(humanChoice, computerChoice){
+    if (humanChoice.toLowerCase() === "rock" && computerChoice === "rock"){
+        return ("Draw! Nobody wins... or you both win?")
+    }
+    else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "rock"){
+        return ("You lose! Rock beats Scissors")
+    }
+    else if (humanChoice.toLowerCase() === "paper" && computerChoice === "rock"){
+        return ("You win! Paper beats Rock")
+    }
+    else if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors"){
+        return ("You win! Rock beats Scissors")
+    }
+    else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "scissors"){
+        return ("Draw! Nobody wins... or you both win?")
+    }
+    else if (humanChoice.toLowerCase() === "paper" && computerChoice === "scissors"){
+        return ("You lose! Scissors beats Paper")
+    }
+    else if (humanChoice.toLowerCase() === "rock" && computerChoice === "paper"){
+        return ("You lose! Paper beats Rock")
+    }
+    else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper"){
+        return ("You win! Scissors beats Paper")
+    }
+    else if (humanChoice.toLowerCase() === "paper" && computerChoice === "paper"){
+        return ("Draw! Nobody wins... or you both win?")
+    }
+};
+
+const playRoundResult = (playRound (humanSelection, computerSelection))
+    if (playRoundResult.includes("You win")){
+         humanScore++;
+    } else if (playRoundResult.includes("You lose")){
+         computerScore++;
+    }
+
+console.log(playRound(humanSelection, computerSelection));
